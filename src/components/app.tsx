a2f2a037343d0800ddcc8ebdbb58bff16a871e6d
Azarak <abbatd@gmail.com>
2022-02-16 03:07:18 +1000

    add SignUp component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -3,11 +3,13 @@ import {
   Routes
 } from 'react-router-dom';
 import SignIn from './sign-in/sign-in';
+import SignUp from './sign-up/sign-up';
 
 export default function App(): JSX.Element {
   return (
     <Routes>
       <Route path={'/signin'} element={<SignIn />} />
+      <Route path={'/signup'} element={<SignUp />} />
     </Routes>
   );
 }
