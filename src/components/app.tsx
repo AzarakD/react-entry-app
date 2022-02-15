c391a9720c0e70e83564cc27a15d4fdc01144848
Azarak <abbatd@gmail.com>
2022-02-16 02:53:10 +1000

    add SignIn component

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,13 @@
-export function App(): JSX.Element {
+import {
+  Route,
+  Routes
+} from 'react-router-dom';
+import SignIn from './sign-in/sign-in';
+
+export default function App(): JSX.Element {
   return (
-    <>
-      Hello
-    </>
+    <Routes>
+      <Route path={'/signin'} element={<SignIn />} />
+    </Routes>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/app';
 // import { store } from './app/store';
 // import { Provider } from 'react-redux';
-import { App } from './components/app';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
     {/* <Provider store={store}> */}
+    <BrowserRouter>
       <App />
+    </BrowserRouter>
     {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
