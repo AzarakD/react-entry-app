84d80c943691f197bcf0acf738fea6594a187330
Azarak <abbatd@gmail.com>
2022-02-17 03:02:48 +1000

    clear database

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { configureStore } from '@reduxjs/toolkit';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
 import { createAPI } from './services/api';
 import { reducer } from './store/reducer';
 import App from './components/app';
